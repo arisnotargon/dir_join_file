2721fbaa7fc059203746d42926bbd4426ac8aa3b
arisnotargon <715535007@qq.com>
2020-08-05 21:15:42 +0900

    add ignore,add test file,add comment

--- decode.py
+++ decode.py
@@ -2,6 +2,7 @@ from hide import Hide
 
 if __name__ == "__main__":
     hide = Hide('password123')
-    encoded = hide.splite('out_20205314919_r.mp4',19012544)
+    # the 2nd param of splite method is the length of the original,after encode.py be executed,you can git it in size.txt
+    encoded = hide.splite('out_20205314919_r.mp4',8033519)
     encoded = hide.decode(encoded)
     hide.unpackage(encoded)
